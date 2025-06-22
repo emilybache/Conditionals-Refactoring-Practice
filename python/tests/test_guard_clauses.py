7bece8c7619bc4a8e05590e77388a87c37d077d8
emily <emily@bacheconsulting.com>
2025-06-22 09:09:31 +0200

    reorganize python project and increase coverage

--- python/tests/test_guard_clauses.py
+++ python/tests/test_guard_clauses.py
@@ -1,9 +1,35 @@
 import pytest
 from guard_clauses import GuardClauses
 
+
 @pytest.fixture
 def guard():
     return GuardClauses()
+
+
+@pytest.mark.parametrize("expected_y,expected_z,input_val", [
+    (4, 3, 3),  # expected y, expected z, input
+    (0, 0, 9)
+])
+def test_introduce_guard_clause_simple(guard, expected_y, expected_z, input_val):
+    guard.y = 0
+    guard.z = 0
+    guard.introduce_guard_clause_simple(input_val)
+    assert guard.y == expected_y
+    assert guard.z == expected_z
+
+
+@pytest.mark.parametrize("expected_y,expected_z,input_val", [
+    (4, 3, 3),  # expected y, expected z, input
+    (0, 0, 9)
+])
+def test_remove_guard_clause_simple(guard, expected_y, expected_z, input_val):
+    guard.y = 0
+    guard.z = 0
+    guard.remove_guard_clause_simple(input_val)
+    assert guard.y == expected_y
+    assert guard.z == expected_z
+
 
 @pytest.mark.parametrize("expected,input_val", [
     (0, 3),
@@ -14,6 +40,7 @@ def test_introduce_guard_clause_single_return(guard, expected, input_val):
     result = guard.introduce_guard_clause_single_return(input_val)
     assert result == expected
 
+
 @pytest.mark.parametrize("expected,input_val", [
     (0, 3),
     (40, 9)
@@ -23,6 +50,7 @@ def test_remove_guard_clause_single_return(guard, expected, input_val):
     result = guard.remove_guard_clause_single_return(input_val)
     assert result == expected
 
+
 @pytest.mark.parametrize("expected,input_val", [
     (0, 3),
     (40, 9)
@@ -31,6 +59,7 @@ def test_remove_guard_clause_multi_return(guard, expected, input_val):
     guard.y = 0
     result = guard.remove_guard_clause_multi_return(input_val)
     assert result == expected
+
 
 @pytest.mark.parametrize("expected,input_val", [
     (0, 3),
