37fa4a0db3a597065fccfae0ef51305ff53c8de5
emily <emily@bacheconsulting.com>
2025-06-23 10:31:47 +0200

    more logical order for exercise

--- python/guard_clauses.py
+++ python/guard_clauses.py
@@ -16,23 +16,6 @@ class GuardClauses:
         # imagine lots more lines of code here including if statements
         self.z = x
 
-    def introduce_guard_clause_single_return(self, x):
-        result = 0
-        if x != 3:
-            self.y = 4
-            # imagine lots more lines of code here including if statements
-            self.z = x
-            result = 4 * x + self.y
-        return result
-
-    def remove_guard_clause_single_return(self, x):
-        if x == 3:
-            return 0
-        self.y = 4
-        # imagine lots more lines of code here including if statements
-        self.z = x
-        return 4 * x + self.y
-
     def introduce_guard_clause_multi_return(self, x):
         if x != 3:
             self.y = 4
@@ -43,6 +26,23 @@ class GuardClauses:
             return 0
 
     def remove_guard_clause_multi_return(self, x):
+        if x == 3:
+            return 0
+        self.y = 4
+        # imagine lots more lines of code here including if statements
+        self.z = x
+        return 4 * x + self.y
+
+    def introduce_guard_clause_single_return(self, x):
+        result = 0
+        if x != 3:
+            self.y = 4
+            # imagine lots more lines of code here including if statements
+            self.z = x
+            result = 4 * x + self.y
+        return result
+
+    def remove_guard_clause_single_return(self, x):
         if x == 3:
             return 0
         self.y = 4
